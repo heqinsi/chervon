1761d2ca6b81176729e629e5a9a992ef40eb9dbf
Muke.Meng <muke,meng@celnet.com.cn>
2017-06-19 21:38:23 +0800

    t

---
+++ src/main/java/com/chervon/iot/EgochervoniotApplication.java
@@ -0,0 +1,12 @@
+package com.chervon.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgochervoniotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgochervoniotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chervon/iot/EgochervoniotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chervon.iot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EgochervoniotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
