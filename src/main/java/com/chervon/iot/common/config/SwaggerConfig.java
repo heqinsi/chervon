8641a76e9be8bf386c10776aec8db5ca2a2f1839
heqinsi <boy.he@celnet.com.cn>
2017-06-20 16:38:26 +0800

    hqs

--- src/main/java/com/chervon/iot/common/config/SwaggerConfig.java
+++ src/main/java/com/chervon/iot/common/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.chervon.iot.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2//启用Swagger2
-
+@RestController
 public class SwaggerConfig {
 
     @Bean
@@ -26,7 +27,7 @@ public class SwaggerConfig {
                 .build();
     }
 
-    @SuppressWarnings("deprecation")
+    //@SuppressWarnings("deprecation")
 	private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Heroku中间件接口测试")
